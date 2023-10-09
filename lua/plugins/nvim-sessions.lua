e9888c20a8efa4f7e5d9bc8f70f266e4e41cd9f3
Carlos Diaz <jdiazmunoz@provectus.com>
2023-10-08 22:21:31 -0500

    adding neovim project plugin

--- lua/plugins/nvim-sessions.lua
+++ lua/plugins/nvim-sessions.lua
@@ -1,112 +1,121 @@
 return {
-  {
-    "folke/persistence.nvim",
-    enabled = false,
-    event = "BufReadPre",
-    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
-  -- stylua: ignore
-  keys = {
-    { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-    { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
-  },
-  },
-  {
-    "rmagatti/auto-session",
-    enabled = true,
-    event = "BufReadPre",
-    opts = {
-      log_level = "error",
-    },
-    keys = {
-      -- {
-      --   "<leader>Sf",
-      --   "<Cmd>Autosession search<Cr>",
-      --   desc = "Load a session",
-      -- },
-      {
-        "<leader>Sd",
-        "<Cmd>Autosession delete<Cr>",
-        desc = "Delete a session",
-      },
-    },
-  },
-  {
-    "rmagatti/session-lens",
-    enabled = true,
-    event = "BufReadPre",
-    opts = {
-      previewer = false,
-      theme_conf = { border = true, bg = false },
-    },
-    keys = {
-      {
-        "<leader>Sf",
-        function()
-          require("session-lens").search_session()
-        end,
-        desc = "Load a session",
-      },
-    },
-  },
-  {
-    "stevearc/resession.nvim",
-    event = "BufReadPre",
-    enabled = false,
-    opts = {
-      autosave = {
-        enabled = true,
-        interval = 60,
-      },
-    },
-    keys = {
-      {
-        "<leader>Sl",
-        function()
-          require("resession").load("last")
-        end,
-        desc = "Load last session",
-      },
-      {
-        "<leader>Ss",
-        function()
-          require("resession").save()
-        end,
-        desc = "Save this session",
-      },
-      {
-        "<leader>St",
-        function()
-          require("resession").save_tab()
-        end,
-        desc = "Save this tab's session",
-      },
-      {
-        "<leader>Sd",
-        function()
-          require("resession").delete()
-        end,
-        desc = "Delete a session",
-      },
-      {
-        "<leader>Sf",
-        function()
-          require("resession").load()
-        end,
-        desc = "Load a session",
-      },
-      {
-        "<leader>S.",
-        function()
-          require("resession").load(vim.fn.getcwd(), { dir = "dirsession" })
-        end,
-        desc = "Load current directory session",
-      },
-    },
-  },
+  -- {
+  --   "rmagatti/auto-session",
+  --   enabled = false,
+  --   event = "BufReadPre",
+  --   opts = {
+  --     log_level = "error",
+  --     auto_session_enabled = true,
+  --     auto_session_create_enabled = true,
+  --     auto_session_enable_last_session = true,
+  --     auto_restore_enabled = true,
+  --   },
+  --   keys = {
+  --     {
+  --       "<leader>Sd",
+  --       "<Cmd>Autosession delete<Cr>",
+  --       desc = "Delete a session",
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "rmagatti/session-lens",
+  --   enabled = false,
+  --   event = "BufReadPre",
+  --   opts = {
+  --     previewer = false,
+  --     theme_conf = { border = true, bg = false },
+  --   },
+  --   keys = {
+  --     {
+  --       "<leader>Sf",
+  --       function()
+  --         require("session-lens").search_session()
+  --       end,
+  --       desc = "Load a session",
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "stevearc/resession.nvim",
+  --   event = "BufReadPre",
+  --   version = false,
+  --   enabled = false,
+  --   opts = {
+  --     autosave = {
+  --       enabled = true,
+  --       cwd = true,
+  --       interval = 60,
+  --       -- Notify when autosaved
+  --       notify = true,
+  --     },
+  --     options = {
+  --       "binary",
+  --       "bufhidden",
+  --       "buflisted",
+  --       "cmdheight",
+  --       "diff",
+  --       "filetype",
+  --       "modifiable",
+  --       "previewwindow",
+  --       "readonly",
+  --       "scrollbind",
+  --       "winfixheight",
+  --       "winfixwidth",
+  --     },
+  --     dir = "resession",
+  --     load_detail = true,
+  --     load_order = "modification_time",
+  --   },
+  --   keys = {
+  --     {
+  --       "<leader>Sl",
+  --       function()
+  --         require("resession").load("last")
+  --       end,
+  --       desc = "Load last session",
+  --     },
+  --     {
+  --       "<leader>Ss",
+  --       function()
+  --         require("resession").save()
+  --       end,
+  --       desc = "Save this session",
+  --     },
+  --     {
+  --       "<leader>St",
+  --       function()
+  --         require("resession").save_tab()
+  --       end,
+  --       desc = "Save this tab's session",
+  --     },
+  --     {
+  --       "<leader>Sd",
+  --       function()
+  --         require("resession").delete()
+  --       end,
+  --       desc = "Delete a session",
+  --     },
+  --     {
+  --       "<leader>Sf",
+  --       function()
+  --         require("resession").load()
+  --       end,
+  --       desc = "Load a session",
+  --     },
+  --     {
+  --       "<leader>S.",
+  --       function()
+  --         require("resession").load(vim.fn.getcwd(), { dir = "dirsession" })
+  --       end,
+  --       desc = "Load current directory session",
+  --     },
+  --   },
+  -- },
   {
     "Shatur/neovim-session-manager",
-    enabled = false,
+    event = "BufReadPre",
+    enabled = true,
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
@@ -146,6 +155,129 @@ return {
           require("session_manager").load_current_dir_session()
         end,
         desc = "Load current directory session",
+      },
+    },
+  },
+  -- {
+  --   "jedrzejboczar/possession.nvim",
+  --   event = "BufReadPre",
+  --   enabled = false,
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   opts = {
+  --     debug = true,
+  --     prompt_no_cr = true,
+  --     telescope = {
+  --       list = {
+  --         default_action = "load",
+  --       },
+  --     },
+  --   },
+  --   keys = {
+  --     {
+  --       "<leader>Ss",
+  --       function()
+  --         -- local fname = vim.fn.input(" name")
+  --         local session = require("possession.session")
+  --         session.save("temp")
+  --       end,
+  --       desc = "Save this session",
+  --     },
+  --     {
+  --       "<leader>Sd",
+  --       function()
+  --         require("telescope").extensions.possession.list({
+  --           default_action = "delete",
+  --         })
+  --       end,
+  --       desc = "Delete a session",
+  --     },
+  --     {
+  --       "<leader>Sf",
+  --       function()
+  --         require("telescope").extensions.possession.list()
+  --       end,
+  --       desc = "Load a session",
+  --     },
+  --     {
+  --       "<leader>S.",
+  --       function()
+  --         require("session_manager").load_current_dir_session()
+  --       end,
+  --       desc = "Load current directory session",
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "gennaro-tedesco/nvim-possession",
+  --   enabled = false,
+  --   dependencies = {
+  --     "ibhagwan/fzf-lua",
+  --   },
+  --   config = true,
+  --   init = function()
+  --     local possession = require("nvim-possession")
+  --     vim.keymap.set("n", "<leader>Sf", function()
+  --       possession.list()
+  --     end, { desc = "Load session" })
+  --     vim.keymap.set("n", "<leader>Ss", function()
+  --       possession.new()
+  --     end, { desc = "Save session" })
+  --     vim.keymap.set("n", "<leader>Su", function()
+  --       possession.update()
+  --     end, { desc = "Update a session" })
+  --     vim.keymap.set("n", "<leader>Sd", function()
+  --       possession.delete()
+  --     end, { desc = "Delete a session" })
+  --   end,
+  -- },
+  -- {
+  --   "olimorris/persisted.nvim",
+  --   enabled = false,
+  --   priority = 900,
+  --   config = true,
+  --   opts = {},
+  -- },
+  {
+    "coffebar/neovim-project",
+    opts = {
+      projects = { -- define project roots
+        "~/.config/neovim",
+        "~/github/personal/wedding/wedding-site/",
+        "~/learning/frontendmasters/*",
+        "~/provectus/*",
+        "~/provectus/supply-chain-core/frontend",
+        "~/provectus/supply-chain-core/backend",
+      },
+      last_session_on_startup = false,
+    },
+    init = function()
+      -- enable saving the state of plugins in the session
+      vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+    end,
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim", tag = "0.1.3" },
+      { "Shatur/neovim-session-manager" },
+    },
+    lazy = false,
+    priority = 100,
+    keys = {
+      {
+        "<leader>Pd",
+        "<Cmd>Telescope neovim-project discover<CR>",
+        desc = "Project discover",
+      },
+      {
+        "<leader>Ph",
+        "<Cmd>Telescope neovim-project history<CR>",
+        desc = "Project history",
+      },
+      {
+        "<leader>Pl",
+        "NeovimProjectLoadRecent",
+        desc = "Load last Project",
       },
     },
   },
